2d15ef06558882dc19f556587cc94afa6c893992
PatilNithin <patilnithinkumarreddy@gamil.com>
2025-06-29 21:17:59 +0530

    final changes has been made

--- dags/e_commerce_etl_dag.py
+++ dags/e_commerce_etl_dag.py
@@ -17,10 +17,8 @@ from google.cloud import storage
 from google.oauth2 import service_account
 from airflow.operators.empty import EmptyOperator
 import pendulum
+import sys
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.join(current_dir, '..') 
-scripts_dir = os.path.join(project_root, 'scripts')
 
 import dataproc_utils as dp_utils
 import data_genarator as dg
